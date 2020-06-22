94b075d430a47fc167965168c6cd83819c70b8e8
Brian Barton <brian.n.barton@gmail.com>
2020-06-22 12:01:25 -0500

    JS conditionals

--- js/conditionals.js
+++ js/conditionals.js
@@ -20,13 +20,43 @@
  * console.logging the function's return value
  */
 
+//
+// var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+//
+// var randomColor = colors[Math.floor(Math.random() * colors.length)];
+//
+//
+// let analyzeColor = (randomColor);
+//
+// switch(analyzeColor) {
+//   case "red":
+//     if ("red") console.log("correct!");
+//     break;
+//   case "green":
+//     if ("blue") console.log("correct!");
+//     break;
+//   case "yellow":
+//     if ("cyan") console.log("do you mean like cayan pepper?");
+//     break;
+//
+//   default:
+//       console.log("Oh no! you guessed wrong!");
+//     break;
+// }
+//
+// analyzeColor = (prompt("pick a color"))
+
 // Don't change the next two lines!
 // These lines create two variables for you:
 // - `colors`: a list of the colors of the rainbow
 // - `randomColor`: contains a single random color value from the list (this
 //                  will contain a different color every time the page loads)
-var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-var randomColor = colors[Math.floor(Math.random() * colors.length)];
+// var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+//
+//
+// var randomColor = colors[Math.floor(Math.random() * colors.length)];
+//
+
 /**
  * TODO:
  * Pass the `randomColor` variable to your function and console.log the results.
@@ -43,9 +73,56 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * Prompt the user for a color when the page loads, and pass the input from the
  * user to your `analyzeColor` function. Alert the return value from your
  * function to show it to the user.
- */
-
-/* ########################################################################## */
+//  */
+// let color = prompt("enter your color")
+// alert(anlyzeColor(color));
+// /* ########################################################################## */
+//Walmart Promotion
+//
+// function calculateTotal(luckyNumber, grandTotal) {
+//   var discountRate = 0;
+//   switch (luckyNumber){
+//     case 0:
+//
+//       discountRate = 0;
+//       break;
+//     case 1:
+//
+//       discountRate = .10;
+//       break;
+//     case 2:
+//
+//       discountRate = .25;
+//       break;
+//     case 3:
+//
+//       discountRate = .35;
+//       break;
+//
+//     case 4:
+//
+//       discountRate = .50;
+//       break;
+//     case 5:
+//
+//       discountRate = 1;
+//       break;
+//   }
+//   return grandTotal - (discountRate * grandTotal);
+// }
+//
+//
+// let luckyNumber = Math.floor(Math.random() * 6);
+//
+// let totalBill = prompt("your current total");
+//
+// alert("We're running a promotion today! here's your lucky number " + luckyNumber );
+//
+// alert("Your previous total is " + totalBill + " and Your new total now comes out to " + calculateTotal(luckyNumber, totalBill).toFixed(2));
+//
+//
+//
+//
 
 /**
  * TODO:
@@ -93,3 +170,21 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * Can you refactor your code to use functions?
  * HINT: The way we prompt for a value could be improved
  */
+
+//parseInt()
+//
+// confirm("would you like to enter a number?");
+// number = parseInt(prompt("enter your number here"));
+// alert("this is your number plus a hundit " + (number + 100));
+
+
+let confirm = confirm("enter a number")
+if (confirm === true) {
+  let confirm = +prompt("enter a number");
+  confirm % 2 === 0 ? alert("thats an even number ") : alert("you entered an odd number");
+  confirm >= 0 ? alert("thats a positive number") : alert("thats a negative number");
+  alert("your number plus 100 " + (confirm + 100)); }
+  else {
+    alert("you chose not to participate")
+
+}
