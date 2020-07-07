d0317461a52664f6b4a1317d04493895df56e373
Brian Barton <brian.n.barton@gmail.com>
2020-07-07 18:56:46 -0500

    update

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -18,18 +18,6 @@
 
 fizzBuzz(30);
 
-  function sleezBuzz(dum) {
-    for (let i=i; i <=dum, i++) {
-      if (i % 15 == 0);
-      console.log("sleezBuzz");
-      else if (i % 5 == 0);
-        console.log("sleez");
-          else if (i % 5 == 0);
-          console.log("buzz");
-          else
-            console.log(i);
-    }
-  }
 
 })();
 
