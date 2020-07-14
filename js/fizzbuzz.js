fc7707e527b2bb095e189ff592d22d0bcc128df7
Brian Barton <brian.n.barton@gmail.com>
2020-07-14 10:48:41 -0500

    today

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -34,35 +34,5 @@ function fizzBuzz(num) {
   }
 }
 
-function fizzbuzz(num) {
-  for (let i = 1; i <= num; i++) {
-    if (i % 15 == 0)
-      console.log("fizzbuzz");
-    else if (i % 3 == 0)
-      console.log("fizz")
-    else if (i % 5 == 0)
-      console.log("buzz");
-    else
-      console.log(i);
-  }
-}
-
-var fibonacci = function (n) {
-  if (n === 1) {
-    return [0, 1];
-  } else {
-    var s = fibonacci(n - 1);
-    s.push(s[s.length - 1] + s[s.length - 2]);
-    return s;
-  }
-};
 //
 // Write a function that takes in a three digit integer and returns the digit in the tens. I.e. isolateTensDigit(345) → 4
-
-function isolateTensDigit(int){
-  int %= 100;
-  int /= 10;
-  int = Math.floor(int);
-  return int
-}
-console.log(isolateTensDigit(345));
