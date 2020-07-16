0c0cae8f0b721a6f9ba74fe701f2a81096c709d0
Brian Barton <brian.n.barton@gmail.com>
2020-07-16 06:45:59 -0500

    update

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,38 +1,66 @@
 (function () {
   "use strict";
 
+  //
+  // function fizzBuzz(num) {
+  //   for (let i = 1; i <= num; i++) {
+  //     if (i % 15 == 0)
+  //       console.log("FizzBuzz");
+  //     else if (i % 3 == 0)
+  //       console.log("Fizz");
+  //     else if (i % 5 == 0)
+  //       console.log("Buzz");
+  //     else
+  //       console.log(i);
+  //   }
+  // }
+  //
+  // fizzBuzz(30);
 
-  function fizzBuzz(num) {
-    for (let i = 1; i <= num; i++) {
-      if (i % 15 == 0)
-        console.log("FizzBuzz");
-      else if (i % 3 == 0)
-        console.log("Fizz");
-      else if (i % 5 == 0)
-        console.log("Buzz");
-      else
-        console.log(i);
-    }
+
+  const myMac = {};
+  myMac.users = [
+    {
+      username: "sophie",
+      id: 1
+    },
+    {
+      username: "vivian",
+      id: 2
+    },
+    {
+      username: "david",
+      id: 3
+    }];
+
+
+
+const findUsers = function (usersArray) {
+  let usernames = [];
+  for (let i=0;i<usersArray.length;i++) {
+    usernames.push(usersArray[i].username);
   }
-
-  fizzBuzz(30);
-
-
-})();
-
-function fizzBuzz(num) {
-  for (let i = 1; i <= num; i++) {
-    if (i % 15 == 0)
-      console.log("FizzBuzz");
-    else if (i % 3 == 0)
-      console.log('Fizz');
-    else if (i % 5 == 0)
-      console.log("Buzz");
-    else
-      console.log(i);
-
-  }
+  return usernames;
 }
+
+
+console.log(findUsers(myMac.users));
+
+
+  const addRole = function(role) {
+    // for (var i=0;i<myMac.users.length;i++) {
+    //     myMac.users[i].role = role;
+    // }
+    myMac.users.forEach(function(item){
+      item.role = role;
+
+
+    })
+  }
+
+  console.log(myMac.users);
+  addRole("instructor");
+  console.log(myMac.users);
 
 //
 // Write a function that takes in a three digit integer and returns the digit in the tens. I.e. isolateTensDigit(345) → 4
