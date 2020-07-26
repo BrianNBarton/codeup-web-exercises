5690bbd06a9ae816bde375d4d0a56aa31b3d328e
Brian Barton <brian.n.barton@gmail.com>
2020-07-26 15:58:08 -0500

    sunday

--- js/weathermap.js
+++ js/weathermap.js
@@ -5,53 +5,39 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
  //q:     "San Antonio, US",
   lat: 29.419,
   lon: -98.4936,
-  units: "imperial"
+  units: "imperial",
+  part: "daily"
 }).done(function(data) {
   console.log(data);
 
   for (let i = 0; i < 5; i++) {
-    let cardId = '#card' + i
+    let cardId = ".card" + i
     //date
 //assign the Date String to #weather-date
     let date = new Date(data.daily[i].dt * 1000);
-    $(cardId + " #weatherDate").html(date.toDateString());
+    $(cardId + " .weatherDate").html(date.toDateString());
     console.log(data.daily[i].dt)
     //temp
     let highTemp = data.daily[i].temp.max;
     let lowTemp = data.daily[i].temp.min;
 //$('#weather-date').html(date.toDateString());
-    $(cardId + "#highLowTemp").html(lowTemp + "°F/ " + highTemp + "°F");
-    console.log(date.daily[i].temp);
+    $(cardId + ".highLowTemp").html(lowTemp + "°F/ " + highTemp + "°F");
+    console.log(data.daily[i].temp);
     //feels like
     let feelsLike = data.daily[i].feels_like.day;
-    $(cardId + " #feelsLike").html(feelsLike + "%");
+    $(cardId + " .feelsLike").html(feelsLike + "%");
     console.log(data.daily[i].feels_like)
     //humidity
     let humidity = data.daily[i].humidity;
-    $(cardId + " #feelsLike").html(humidity + "°F");
+    $(cardId + " .feelsLike").html(humidity + "°F");
     console.log(data.daily[i].humidity)
     //
     let wind = data.daily[i].wind;
-    $(cardId + " #wind").html(wind + " MPH");
+    $(cardId + " .wind").html(wind + " MPH");
     console.log(data.daily[i].wind_speed)
     //
     let pressure = data.daily[i].pressure;
-    $(cardId + " #pressure").html(pressure + " mBAR");
+    $(cardId + " .pressure").html(pressure + " mBAR");
     console.log(data.daily[i].pressure)
-
-//$('#weather-date').html(date.toDateString());
-//$('#high-low-temp').html(lowTemp + '°F/ ' + highTemp + '°F');
-
-
   }
 });
-
-//
-// $('#high-low-temp').click(function(){
-//   $(this).css('color', 'blue');
-//   // border
-//   // border radius
-//   // padding
-//   // background
-
-//$(' #=id selector (id, class, tag) ').
