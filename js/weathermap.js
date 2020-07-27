091647abca1006b6bffdec960b0df310d2fe2284
Brian Barton <brian.n.barton@gmail.com>
2020-07-27 10:50:08 -0500

    update

--- js/weathermap.js
+++ js/weathermap.js
@@ -1,4 +1,4 @@
-
+//
 
 $.get("http://api.openweathermap.org/data/2.5/onecall", {
   APPID: OpenWeatherKey,
@@ -9,35 +9,46 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
   part: "daily"
 }).done(function(data) {
   console.log(data);
-
   for (let i = 0; i < 5; i++) {
-    let cardId = ".card" + i
+    let cardId = "#card-" + i
     //date
 //assign the Date String to #weather-date
     let date = new Date(data.daily[i].dt * 1000);
-    $(cardId + " .weatherDate").html(date.toDateString());
+    $("#weatherDate-" + i).html(date.toDateString());
     console.log(data.daily[i].dt)
     //temp
     let highTemp = data.daily[i].temp.max;
     let lowTemp = data.daily[i].temp.min;
 //$('#weather-date').html(date.toDateString());
-    $(cardId + ".highLowTemp").html(lowTemp + "°F/ " + highTemp + "°F");
+    $("#highLowTemp-" + i).html(lowTemp + "°F/ " + highTemp + "°F");
     console.log(data.daily[i].temp);
     //feels like
     let feelsLike = data.daily[i].feels_like.day;
-    $(cardId + " .feelsLike").html(feelsLike + "%");
+    $("#feelsLike-" + i).html(feelsLike + "%");
     console.log(data.daily[i].feels_like)
     //humidity
     let humidity = data.daily[i].humidity;
-    $(cardId + " .feelsLike").html(humidity + "°F");
+    $("#humidity-" + i).html(humidity + "°F");
     console.log(data.daily[i].humidity)
     //
-    let wind = data.daily[i].wind;
-    $(cardId + " .wind").html(wind + " MPH");
+    let wind = data.daily[i].wind_speed;
+    $("#wind-" + i).html(wind + " MPH");
     console.log(data.daily[i].wind_speed)
     //
     let pressure = data.daily[i].pressure;
-    $(cardId + " .pressure").html(pressure + " mBAR");
+    $("#pressure-" + i).html(pressure + " mBAR");
     console.log(data.daily[i].pressure)
   }
 });
+
+
+mapboxgl.accessToken = mapboxKey;
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v9',
+  zoom: 10,
+  center: [-98.4916, 29.4252]
+});
+
+
+
