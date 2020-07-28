4b3814daf7ac5cba444ca0bdcc1e954d5e78b212
Brian Barton <brian.n.barton@gmail.com>
2020-07-28 16:19:09 -0500

    Finished weathermap

--- js/weathermap.js
+++ js/weathermap.js
@@ -1,61 +1,69 @@
 
+function updateWeather(lat, lon) {
+  $.get("https://api.openweathermap.org/data/2.5/onecall", {
+    APPID: OpenWeatherKey,
+    //q:     "San Antonio, US",
+    lat: lat,
+    lon: lon,
+    units: "imperial",
+    part: "daily"
 
-$.get("http://api.openweathermap.org/data/2.5/onecall", {
-  APPID: OpenWeatherKey,
- //q:     "San Antonio, US",
-  lat: 29.419,
-  lon: -98.4936,
-  units: "imperial",
-  part: "daily"
 
+  }).done(function (data) {
+    console.log(data);
 
-}).done(function(data) {
-  console.log(data);
-  for (let i = 0; i < 5; i++) {
-    let cardId = "#card-" + i
-    //date
+    reverseGeocode(lat, lon);
+    for (let i = 0; i < 5; i++) {
+      let cardId = "#card-" + i
+      //date
 //assign the Date String to #weather-date
-    let date = new Date(data.daily[i].dt * 1000);
-    $("#weatherDate-" + i).html(date.toDateString());
-    console.log(data.daily[i].dt)
-    //temp
-    let highTemp = data.daily[i].temp.max;
-    let lowTemp = data.daily[i].temp.min;
+      let date = new Date(data.daily[i].dt * 1000);
+      $("#weatherDate-" + i).html(date.toDateString());
+      console.log(data.daily[i].dt)
+      //temp
+      let highTemp = data.daily[i].temp.max;
+      let lowTemp = data.daily[i].temp.min;
 //$('#weather-date').html(date.toDateString());
-    $("#highLowTemp-" + i).html(lowTemp + "°F/ " + highTemp + "°F");
-    console.log(data.daily[i].temp);
-    //feels like
-    let feelsLike = data.daily[i].feels_like.day;
-    $("#feelsLike-" + i).html(feelsLike + "%");
-    console.log(data.daily[i].feels_like)
-    //humidity
-    let humidity = data.daily[i].humidity;
-    $("#humidity-" + i).html(humidity + "°F");
-    console.log(data.daily[i].humidity)
-    //
-    let wind = data.daily[i].wind_speed;
-    $("#wind-" + i).html(wind + " MPH");
-    console.log(data.daily[i].wind_speed)
-    //
-    let pressure = data.daily[i].pressure;
-    $("#pressure-" + i).html(pressure + " mBAR");
-    console.log(data.daily[i].pressure)
-  }
-});
+      $("#highLowTemp-" + i).html(lowTemp + "°F/ " + highTemp + "°F");
+      console.log(data.daily[i].temp);
+      //feels like
+      let feelsLike = data.daily[i].feels_like.day;
+      $("#feelsLike-" + i).html(feelsLike + "%");
+      console.log(data.daily[i].feels_like)
+      //humidity
+      let humidity = data.daily[i].humidity;
+      $("#humidity-" + i).html(humidity + "°F");
+      console.log(data.daily[i].humidity)
+      //
+      let wind = data.daily[i].wind_speed;
+      $("#wind-" + i).html(wind + " MPH");
+      console.log(data.daily[i].wind_speed)
+      //
+      let pressure = data.daily[i].pressure;
+      $("#pressure-" + i).html(pressure + " mBAR");
+      console.log(data.daily[i].pressure)
+    }
+  });
+}
 
-mapboxgl.accessToken = mapboxKey;
-var map = new mapboxgl.Map({
-  container: 'map', // container id
-  style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-  center: [-98.4936, 29.419], // starting position [lng, lat]
-  zoom: 10 // starting zoom
-});
+  mapboxgl.accessToken = mapboxKey;
+  var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [-98.4936, 29.419], // starting position [lng, lat]
+    zoom: 10 // starting zoom
+  });
 
-var marker = new mapboxgl.Marker({
-  draggable: true
-})
-  .setLngLat([-98.493, 29.419])
-  .addTo(map);
+  var marker = new mapboxgl.Marker({
+    draggable: true
+  })
+    .setLngLat([-98.493, 29.419])
+    .addTo(map);
+
+  marker.on('dragend', onDragEnd);
+  updateWeather(29.419, -98.493)
+
+
 
 function onDragEnd() {
   var lngLat = marker.getLngLat();
@@ -63,6 +71,8 @@ function onDragEnd() {
   coordinates.innerHTML =
     'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
 
+  console.log(lngLat);
+  updateWeather(lngLat.lat, lngLat.lng);
 // take .done contents into own function
   // send data variable from .done to new method
   // make another ajax call inside on dragend using new longlat
@@ -71,7 +81,6 @@ function onDragEnd() {
 
 }
 
-marker.on('dragend', onDragEnd);
 
 const forecastWeather = function (latitude, longitude) {
   $.get("https://api.openweathermap.org/data/2.5/onecall", {
@@ -85,6 +94,16 @@ const forecastWeather = function (latitude, longitude) {
 
 function NewLocation(){
 
+}
+
+function reverseGeocode(lat, lon){
+$.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lon},${lat}.json`, {
+  types: "place",
+  access_token: mapboxKey
+}).done(function(data){
+  $('#place').text(data.features[0].place_name);
+  console.log(data);
+})
 }
 
 //Once you’ve acquired the new Latitude/Longitude after the marker has been moved,
